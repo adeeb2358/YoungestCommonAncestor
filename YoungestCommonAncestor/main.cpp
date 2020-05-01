9eec346245479ee75fbccfc01d2baa606ac39189
Adeeb Mohammed <adeeb2358@gmail.com>
2020-05-01 22:05:36 +0900

    Initial Commit

---
+++ YoungestCommonAncestor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  YoungestCommonAncestor
+//
+//  Created by adeeb mohammed on 01/05/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
